4f9ecfe5395030fd358d00db2c2afcc0f76e0623
Gael Castro <gaelcastrolara2006@gmail.com>
2026-01-25 16:55:42 -0600

    downloader

--- act2.js
+++ act2.js
@@ -1,1 +1,74 @@
-alert("BIENVENIDO AL GESTOR DE DESCARGAS DE EPIC GAMES!");
+
+class Juego {
+    constructor(nombre, imagen) {
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.elemento = this.crearElemento();
+    }
+
+    crearElemento() {
+        const nuevaTarjeta = document.createElement('div');
+        nuevaTarjeta.className = 'game-card';
+        
+        nuevaTarjeta.innerHTML = `<div class="card-body">
+                <div class="poster-container">
+                    <img src="${this.imagen}" alt="${this.nombre}" class="poster">
+                </div>
+                <div class="game-details">
+                    <div class="game-header">
+                        <h3>${this.nombre}</h3>
+                    </div>
+                    <div class="status-row">
+                        <span class="status-text">Actualizacion en espera</span>
+                    </div>
+                </div>
+            </div>
+            <div class="card-controls">
+                <button class="control-btn btn-close">X</button>
+                <button class="control-btn">...</button>
+            </div>
+        `;
+
+        return nuevaTarjeta;
+    }
+
+    eliminar() {
+        this.elemento.remove();
+    }
+}
+
+class GestorDescargas {
+    constructor(contenedorId) {
+        this.contenedor = document.getElementById(contenedorId);
+        this.EmpezarEventos();
+        alert("BIENVENIDO AL GESTOR DE DESCARGAS DE EPIC GAMES!");
+    }
+
+    EmpezarEventos() {
+        document.getElementById('btn-agregar').addEventListener('click', () => this.agregarDescarga());
+        document.getElementById('btn-eliminar').addEventListener('click', () => this.eliminarDescarga());
+    }
+
+    agregarDescarga() {
+        const nombreJuego = prompt("Ingrese el nombre del juego:");
+        const urlImagen = prompt("Ingrese la URL de la imagen del juego:");
+        if (nombreJuego && urlImagen) {
+            const nuevoJuego = new Juego(nombreJuego, urlImagen);
+            this.contenedor.appendChild(nuevoJuego.elemento);
+        } else {
+            alert("Nombre del juego o URL de la imagen no proporcionados.");
+        }
+    }
+
+    eliminarDescarga() {
+        const tarjetas = this.contenedor.getElementsByClassName('game-card');
+        if (tarjetas.length > 0) {
+            tarjetas[tarjetas.length - 1].remove();
+        } else {
+            alert("No hay descargas para eliminar.");
+        }
+    }
+}
+
+const iniciarGestor = new GestorDescargas('container-descargas');
+        
